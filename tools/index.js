fa629acb9881432f46bf021e6f57b96225b63ef6
adam mao <saalih@qq.com>
2013-12-13 23:45:05 +0800

    index

---
+++ tools/index.js
@@ -0,0 +1,4 @@
+var $ = require("jquery")
+$.getJSON("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx05940045fd60e0f1&secret=fff287f1c1a4e9bb2cf20cdb2e0a5b85",function(json){
+   console.log("hehe");
+})
