ca5883bc6701db1c2e2b856fa413940e8308b400
adam mao <saalih@qq.com>
2013-12-15 10:37:28 +0800

    test

--- php/cgi/jump_with_id/index.php
+++ php/cgi/jump_with_id/index.php
@@ -1,8 +1,34 @@
 <?php
+require_once(dirname(__FILE__)."/../../config.php");
 /**
  * Created by PhpStorm.
  * User: aohajin
  * Date: 12/15/13
  * Time: 9:53 AM
  */
-echo $_SERVER['QUERY_STRING'];
+
+$code = $_GET['code'];
+$state = $_GET['state'];
+
+parse_str($state);// $eid $appid $t
+
+$db = \mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Database connect failed: ".mysql_error());
+\mysql_select_db(DB_DATABASENAME, $db);
+\mysql_query("set names utf8");
+
+$result = \mysql_query('select app_key from Estate where app_id='.$appid.' limit 1', $db);
+
+$arr = null;
+if ($result){
+    $arr = \mysql_fetch_array($result);
+}
+
+\mysql_close($db);
+$appkey = $arr['app_key'];
+
+// access token
+//https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code
+
+$token = file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid='
+.$appid.'&secret='.$appkey.'&code='.$code.'&grant_type=authorization_code");
+var_dump($token);
