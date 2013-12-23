dddf54b722ed356c056c949a1feed93d683fd965
adam mao <saalih@qq.com>
2013-12-23 23:57:05 +0800

    stuff

--- php/template/sub-ad.js.php
+++ php/template/sub-ad.js.php
@@ -1,20 +1,32 @@
-renderData({ "bgImg" : "../img/ad-bg.jpg",
-"desc" : "<?php echo $intro['desc']?>",
-"images" : [
+renderData({
+    "bgImg" : "../img/ad-bg.jpg",
+    "contact" : { "phones" : [ "",""],
+    "timesOn" : ""
+},
+    "desc" : "<?php echo $intro['desc']?>",
+    "images" : [
 <?php if(is_array($intro['imgs']))foreach($intro['imgs'] as $img){ ?>
-{ "title" : "<?php echo $img['title']?>",
-"url" : "<?php echo $img['url']?>"},
+        {
+            "title" : "<?php echo $img['title']?>",
+            "url" : "<?php echo $img['url']?>"
+        },
 <?php } ?>
-],
-"modules" : [ { "items" : [ { "actid" : "10001",
-"desc" : "生命排毒3天2夜套餐",
-"icon" : "",
-"timeEnd" : "23:00",
-"timeStart" : "9:00",
-"title" : "探寻健康·排毒之旅套餐1"
-} ],
-"title" : "尊享优惠"
-} ],
-"ytName" : "狮子湖健康中心",
-"ytid" : "1001"
+    ],
+    "modules" : [
+        {
+            "items" : [
+                {
+                    "actid" : "",
+                    "desc" : "",
+                    "icon" : "",
+                    "timeEnd" : "23:00",
+                    "timeStart" : "9:00",
+                    "title" : ""
+                }
+            ],
+            "title" : "尊享优惠"
+        }
+    ],
+    "ytName" : "<?php echo $intro['name']?>",
+    "ytid" : "<?php echo $id?>"
 });
